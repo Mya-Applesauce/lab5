d55eff22c383b39826815a84c6a15c9a1cc01969
Ari Papke <myaapplesauce42@gmail.com>
2026-02-14 17:34:57 -0500

    Add files via upload

--- lab5_apapke-1.py
+++ lab5_apapke-1.py
@@ -9,32 +9,38 @@ import random
 
 die_results = [1,2,3,4,5,6]
 
-while input != quit:
+keep_playing = ""
+
+while keep_playing != "quit":
     keep_playing = input("Ready to roll?")
     if keep_playing == "yes" or "Yes" or "yeah" or "sure":
         result_1 = random.choice(die_results)
         result_2 = random.choice(die_results)
-        if (result_1 + result_2) == 2:
-            print(f"Snake Eyes")
-        elif (result_1 + result_2) == 3:
-            print(f"Ace Caught a Deuce")
-        elif (result_1 + result_2) == 4:
-            print(f"Little Joe from Kokomo")
-        elif (result_1 + result_2) == 5:
-            print(f"Little Phoebe")
-        elif (result_1 + result_2) == 6:
-            print(f"Jimmy Hicks from the Sticks")
-        elif (result_1 + result_2) == 7:
-            print(f"Six Ace")
-        elif (result_1 + result_2) == 8:
-            print(f"Eighter from Decatur")
-        elif (result_1 + result_2) == 9:
-            print(f"Nina from Pasadena")
-        elif (result_1 + result_2) == 10:
-            print(f"Puppy Paws")
-        elif (result_1 + result_2) == 11:
-            print(f"Six Five no Jive")
-        elif (result_1 + result_2) == 12:
-            print(f"Boxcars")
-    elif keep_playing == "no":
-        break
+        if keep_playing != "no":
+            if (result_1 + result_2) == 2:
+                print(f"Snake Eyes")
+            elif (result_1 + result_2) == 3:
+                print(f"Ace Caught a Deuce")
+            elif (result_1 + result_2) == 4:
+                print(f"Little Joe from Kokomo")
+            elif (result_1 + result_2) == 5:
+                print(f"Little Phoebe")
+            elif (result_1 + result_2) == 6:
+                print(f"Jimmy Hicks from the Sticks")
+            elif (result_1 + result_2) == 7:
+                print(f"Six Ace")
+            elif (result_1 + result_2) == 8:
+                print(f"Eighter from Decatur")
+            elif (result_1 + result_2) == 9:
+                print(f"Nina from Pasadena")
+            elif (result_1 + result_2) == 10:
+                print(f"Puppy Paws")
+            elif (result_1 + result_2) == 11:
+                print(f"Six Five no Jive")
+            elif (result_1 + result_2) == 12:
+                print(f"Boxcars")
+        else:
+            print("See ya later!")
+            break
+
+        
